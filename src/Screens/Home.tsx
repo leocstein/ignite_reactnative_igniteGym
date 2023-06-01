da702797ff1dc1e77b1105e94050b9c1f28100a2
Leonardo <leonardo.stenio@gmail.com>
2023-06-01 00:04:27 -0300

    Create UserPhoto and Group components

--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -1,10 +1,37 @@
+import { useState } from "react";
+import { FlatList, VStack } from "native-base";
 import { HomeHeader } from "@components/HomeHeader";
-import { Center, Text, VStack } from "native-base";
+import { Group } from "@components/Group";
 
 export function Home() {
+  const [groups, setGroups] = useState([
+    "costas",
+    "biceps",
+    "triceps",
+    "ombro",
+  ]);
+  const [groupSelected, setGroupSelected] = useState("costa");
+
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name="costa"
+            isActive={groupSelected === "costa"}
+            onPress={() => setGroupSelected("costa")}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
     </VStack>
   );
 }
--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,12 +1,29 @@
-import { HStack, Heading, Text, VStack } from "native-base";
+import { TouchableOpacity } from "react-native";
+import { HStack, Heading, Icon, Text, VStack } from "native-base";
+import { MaterialIcons } from "@expo/vector-icons";
+
+import { UserPhoto } from "./UserPhoto";
 
 export function HomeHeader() {
   return (
-    <HStack>
-      <VStack>
-        <Text color="gray.100">Olá,</Text>
-        <Heading color="gray.100">Leonardo</Heading>
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto
+        source={{ uri: "https://github.com/leocstein.png" }}
+        alt="Imagem do usuário"
+        size={16}
+        mr={4}
+      />
+      <VStack flex={1}>
+        <Text color="gray.100" fontSize="md">
+          Olá,
+        </Text>
+        <Heading color="gray.100" fontSize="md">
+          Leonardo
+        </Heading>
       </VStack>
+      <TouchableOpacity>
+        <Icon as={MaterialIcons} name="logout" color="gray.200" size={7} />
+      </TouchableOpacity>
     </HStack>
   );
 }
