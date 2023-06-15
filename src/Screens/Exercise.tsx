76d2ecdfa1480eec69b726de48b94c2a230a1068
leonardo <leonardo.stenio@gmail.com>
2023-06-15 17:36:09 -0300

    Implement header Exercise screen

--- src/Screens/Exercise.tsx
+++ src/Screens/Exercise.tsx
@@ -1,9 +1,61 @@
-import { Center, Text } from "native-base";
+import { Box, HStack, Heading, Icon, Image, Text, VStack } from "native-base";
+import { TouchableOpacity } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { AppNavigatorRoutesProps } from "@routes/app.routes";
+import BodySvg from "@assets/body.svg";
 
 export function Exercise() {
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
   return (
-    <Center flex={1}>
-      <Text color="white">Exercise</Text>
-    </Center>
+    <VStack flex={1}>
+      <VStack px={8} bg="gray.600" pt={12}>
+        <TouchableOpacity onPress={handleGoBack}>
+          <Icon as={Feather} name="arrow-left" color="green.500" size={6} />
+        </TouchableOpacity>
+
+        <HStack
+          justifyContent="space-between"
+          mt={4}
+          mb={8}
+          alignItems="center"
+        >
+          <Heading color="gray.100" fontSize="lg" flexShrink={1}>
+            Puxada frontal
+          </Heading>
+
+          <HStack alignItems="center">
+            <BodySvg />
+            <Text color="gray.200" ml={1} textTransform="capitalize">
+              Costas
+            </Text>
+          </HStack>
+        </HStack>
+      </VStack>
+
+      <VStack p={8}>
+        <Image
+          w="full"
+          h={80}
+          source={{
+            uri: "https://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg",
+          }}
+          alt="Nome do exercício"
+          resizeMode="cover"
+          rounded="lg"
+        />
+
+        <Box>
+          <HStack>
+            <HStack></HStack>
+          </HStack>
+        </Box>
+      </VStack>
+    </VStack>
   );
 }
